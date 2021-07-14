22c219a0eec2b8bf5e5aed01cf7fef39cf968431
Chun TaeKyung <ctk0327@Naver.com>
2021-07-15 02:31:22 +0900

    Time: 152 ms (100.00%), Space: 39.3 MB (21.00%) - LeetHub

---
+++ counting-bits/counting-bits.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun countBits(n: Int): IntArray {
+        val dp = IntArray(n + 1);
+        for (i in 1..n) {
+            dp[i] = dp[i shr 1] + i % 2;
+        }
+        return dp;
+    }
+}
