0669d33cddda4fe95e58ac4c5754a1028b0db0c7
Chun TaeKyung <ctk0327@Naver.com>
2021-08-03 02:13:40 +0900

    Update subsets.kt

--- subsets/subsets.kt
+++ subsets/subsets.kt
@@ -2,27 +2,16 @@ import kotlin.math.pow
 
 class Solution {
     fun subsets(nums: IntArray): List<List<Int>>? {
-        val result = mutableListOf<Int>();
-        val numTranslate = nums.map { a -> a + 10 }.toIntArray();
-        fun makeArray(input: IntArray, output: Int) {
+        val result = mutableListOf<List<Int>>();
+        fun makeArray(input: IntArray, output: IntArray) {
             if (input.isEmpty()) {
-                result.add(output)
+                result.add(output.toMutableList());
                 return;
             }
-            val now = input[0];
-            makeArray(input.drop(1).toIntArray(), output or 2.0.pow(now).toInt());
+            makeArray(input.drop(1).toIntArray(), output.plus(input[0]));
             makeArray(input.drop(1).toIntArray(), output);
         }
-        makeArray(numTranslate, 0);
-        return result.map { a ->
-            val binary = Integer.toBinaryString(a).reversed();
-            val result = mutableListOf<Int>();
-            binary.toCharArray().forEachIndexed { index, c ->
-                if (c == '1') {
-                    result.add(index-10)
-                }
-            }
-            return@map result;
-        }
+        makeArray(nums, IntArray(0));
+        return result;
     }
 }
