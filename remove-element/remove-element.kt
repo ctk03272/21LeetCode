a809dc03230759edb627677176dffe0f2927acf0
Chun TaeKyung <ctk0327@Naver.com>
2021-08-06 01:28:03 +0900

    Time: 156 ms (97.08%), Space: 34.8 MB (71.75%) - LeetHub

---
+++ remove-element/remove-element.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var index = 0;
+        for (i in nums.indices) {
+            if (nums[i] != `val`) {
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
