4b8e5f3252e4215df2a73b0a515c502c7b6ddef2
Chun TaeKyung <ctk0327@Naver.com>
2021-09-26 11:44:31 +0900

    Time: 211 ms (18.24%), Space: 47.3 MB (5.13%) - LeetHub

---
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let small=0;
+    let big=numbers.length-1;
+    if(numbers[small]+numbers[big]==target){
+        return [small+1,big+1]
+    }
+    
+    while(small<big){
+        console.log(`${small} ${big}`)
+        if(numbers[small]+numbers[big]==target){
+            return [small+1,big+1]
+        }
+        
+        if(numbers[small]+numbers[big]<target){
+            small++;
+        }else{
+            big--;
+        }
+    }
+};
