2fe1df99ffac5d81ee6971a2795cf12aba090289
Chun TaeKyung <ctk0327@Naver.com>
2022-01-09 10:41:17 +0900

    Time: 104 ms (60.97%), Space: 43 MB (34.57%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,9 @@
+var replaceElements = function (arr) {
+    let max = -1
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let temp = Math.max(max, arr[i]);
+        arr[i]=max;
+        max=temp;
+    }
+    return arr;
+};
