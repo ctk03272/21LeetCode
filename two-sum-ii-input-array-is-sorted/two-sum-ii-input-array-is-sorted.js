9ee8ea51450bccc73e6b083337487617645d388d
Chun TaeKyung <ctk0327@Naver.com>
2022-01-23 22:45:46 +0900

    Update two-sum-ii-input-array-is-sorted.js

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -5,13 +5,8 @@
  */
 var twoSum = function(numbers, target) {
     let small=0;
-    let big=numbers.length-1;
-    if(numbers[small]+numbers[big]==target){
-        return [small+1,big+1]
-    }
-    
+    let big=numbers.length-1;    
     while(small<big){
-        console.log(`${small} ${big}`)
         if(numbers[small]+numbers[big]==target){
             return [small+1,big+1]
         }
