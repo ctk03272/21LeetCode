73e89db94fb34d821cd9b6ef7a6d8bb7a7d89507
Chun TaeKyung <ctk0327@Naver.com>
2022-03-06 23:53:28 +0900

    Time: 119 ms (13.95%), Space: 42.9 MB (5.62%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    var start=0;
+    var end=nums.length;
+    var result=end;
+    while(start<end){
+        var mid=parseInt((start+end)/2);
+        if(nums[mid]==target){
+            result=mid;
+            break;
+        }else if(nums[mid]<target){
+            start=mid+1;
+        }else if(nums[mid]>target){
+            end=mid;
+            result=end;
+        }
+    }
+    return result;
+    
+};
