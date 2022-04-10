5078ef1f2a178913067f5f0f255e56f2b7bc806a
Chun TaeKyung <ctk0327@Naver.com>
2022-04-11 01:19:50 +0900

    Update 207-course-schedule.js

--- 207-course-schedule/207-course-schedule.js
+++ 207-course-schedule/207-course-schedule.js
@@ -1,45 +1,49 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
 var canFinish = function(numCourses, prerequisites) {
-    
-    const preMap = {};
-    const visited = {};
-    
-	// build the adacency list
-    for(let i = 0; i< prerequisites.length; i++){
-        if(preMap[prerequisites[i][0]] === undefined){
-            preMap[prerequisites[i][0]] = [prerequisites[i][1]]
-        } else {
-            preMap[prerequisites[i][0]].push(prerequisites[i][1])
-        }
-    }
-    console.log(preMap)
-    const dfs = (node) => {
-        if(visited[node]){
-            return false;
-        }
-        if(preMap[node] !==undefined){
-            if (preMap[node].length === 0){
-                return true;
+    let stack=[];
+    let adj=Array.from(Array(numCourses+1), ()=>Array());
+    let visited=Array.from(numCourses+1).fill(0);
+    let map=new Map();
+
+    let dfs=function(start){
+        visited[start]=1;
+        let index=map.has(start)?map.get(start)+1:1;
+        map.set(start,index)
+        adj[start].forEach(a=>{
+            if(!visited[a]){
+                dfs(a);
             }
-        
-            visited[node] = true;
-            for(let val of preMap[node]){
-                if(!dfs(val)){
-                    return false
+        });
+    };
+
+    let check=function(){
+        let ans=false;
+        for(let i=0;i<numCourses;i++){
+            adj[i].forEach(a=>{
+                if(map.has(i)?map.get(i):0>map.has(a)?map.get(a):0){
+                    ans=true;
                 }
-            }
-            visited[node] = false;
-            
-            preMap[node] = [];
+            })
         }
-        return true;
+        return ans;
+    };
 
+    for(let i=0;i<prerequisites.length;i++){
+        let now=prerequisites[i][0];
+        let pre=prerequisites[i][1];
+        adj[now].push(pre);
     }
-    
-    for(let key in preMap){
-        if(!dfs(key)){
-            return false
+
+    for(let i=0;i<numCourses;i++){
+        if(!visited[i]){
+            dfs(i,null);
         }
     }
-    return true
-
+    return !check();
 };
+
+canFinish(5,[[1,4],[2,4],[3,1],[3,2]]);
