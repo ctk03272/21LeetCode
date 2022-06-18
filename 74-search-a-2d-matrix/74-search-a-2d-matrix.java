f39ffd3c1fae500d5e6c60f025b1467ab014555b
Chun TaeKyung <ctk0327@Naver.com>
2022-06-18 15:25:02 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (94.40%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,26 +1,15 @@
 class Solution {
 	public boolean searchMatrix(int[][] matrix, int target) {
-		int targetRow=findSearchRow(matrix,target);
-		if(targetRow<0){
-			return false;
+		int i = 0, j = matrix[0].length - 1;
+		while (i < matrix.length && j >= 0) {
+			if (matrix[i][j] == target) {
+				return true;
+			} else if (matrix[i][j] < target) {
+				i++;
+			} else if (matrix[i][j] > target) {
+				j--;
+			}
 		}
-		int[] getRow = matrix[targetRow];
-		return Arrays.binarySearch(getRow,target)<0?false:true;
-	}
-
-	public int findSearchRow(int[][] matrix, int target) {
-		int low = 0, high = matrix.length - 1;
-		while (low <= high) {
-			int mid = (low + high) >>> 1;
-			long midVal = matrix[mid][0];
-
-			if (midVal < target)
-				low = mid + 1;
-			else if (midVal > target)
-				high = mid - 1;
-			else
-				return mid; // key found
-		}
-		return Math.min(low, high);
+		return false;
 	}
 }
