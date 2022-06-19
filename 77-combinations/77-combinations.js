9c80bc9115e2a97eab1757e82d1932dfa15ad4f6
Chun TaeKyung <ctk0327@Naver.com>
2022-06-19 11:33:39 +0900

    Time: 136 ms (76.10%), Space: 49 MB (39.12%) - LeetHub

---
+++ 77-combinations/77-combinations.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+    let findAnswer = function (n, k) {
+        let ans = [];
+        if (k > n || k < 0) {
+            return ans;
+        }
+        if (k == 0) {
+            ans.push([]);
+            return ans;
+        }
+        ans = findAnswer(n - 1, k - 1);
+        ans.forEach(a => a.push(n));
+        let temp=findAnswer(n - 1, k);
+        if(temp.length>0){
+            ans.push(...temp);
+        }
+        return ans;
+    }
+    return findAnswer(n,k);
+};
