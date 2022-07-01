7ff43728f90fd3c87ccf0b1bf7bfbd4e961f5547
Chun TaeKyung <ctk0327@Naver.com>
2022-07-02 00:15:00 +0900

    Time: 347 ms (8.84%), Space: 39.1 MB (5.82%) - LeetHub

---
+++ 66-plus-one/66-plus-one.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        var length = digits.size;
+        var add = 1
+        for (i: Int in length - 1 downTo 0) {
+            var temp = digits[i] + add
+            if (temp == 10) {
+                add = 1
+                temp = 0
+            } else {
+                add = 0
+            }
+            digits[i] = temp
+        }
+        if (add==1){
+            return digits.reversed().plus(add).reversed().toIntArray();
+        }
+        return digits;
+    }
+}
